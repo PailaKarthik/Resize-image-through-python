e53bb7374a396b261d7daa61d888cf96e44d4346
Paila Karthik <107779653+PailaKarthik@users.noreply.github.com>
2024-10-19 17:19:15 +0530

    Add files via upload

---
+++ imageResize.py
@@ -0,0 +1,17 @@
+import cv2
+
+#configurable paramaters
+source='siva.png'
+destination='siva1.png'
+scale_percent=50
+
+src=cv2.imread(source,cv2.IMREAD_UNCHANGED)
+
+#percent by which the image is resized 
+new_width=int(src.shape[1] * scale_percent /100)
+new_height=int(src.shape[0] * scale_percent /100)
+
+#resize image
+output=cv2.resize(src,(new_width,new_height))
+cv2.imwrite(destination,output)
+# cv2.waitKey(0)
